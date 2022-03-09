d0b760db0a3fc55cc340c740795cfb4bd744e2ec
nguyenhieu208 <hieuitkma@gmail.com>
2022-03-09 22:22:45 +0700

    feat: file system routing

---
+++ pages/posts/create.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export interface CreatePageProps {
+}
+
+export default function CreatePage (props: CreatePageProps) {
+  return (
+    <div>
+      Create page
+    </div>
+  );
+}
