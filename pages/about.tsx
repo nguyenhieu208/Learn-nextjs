a3def06480894d317c9375f781b75a5530ec426e
nguyenhieu208 <hieuitkma@gmail.com>
2022-03-10 22:53:25 +0700

    feat: 10 automatic static optimization

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,12 +1,23 @@
+import { useRouter } from 'next/router';
 import React from 'react';
 
 export interface AboutProps {
 }
 
 export default function About (props: AboutProps) {
+  const router = useRouter();
+
+  console.log('About query: ', router.query);
+
   return (
     <div>
       About page
     </div>
   );
+}
+
+export async function getServerSideProps() {
+  return {
+    props: {}, // will be passed to the page component as props
+  }
 }
