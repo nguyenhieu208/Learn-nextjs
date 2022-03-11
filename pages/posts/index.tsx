b4598499d6656b8b8244c5870a13661a8cebf05b
nguyenhieu208 <hieuitkma@gmail.com>
2022-03-11 09:18:01 +0700

    11 getStaticProps with TS

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,12 +1,40 @@
+import { GetStaticProps, GetStaticPropsContext } from 'next';
 import React from 'react';
 
 export interface PostListPageProps {
+  posts: any[]
 }
 
-export default function PostListPage (props: PostListPageProps) {
+export default function PostListPage ({posts}: PostListPageProps) {
   return (
     <div>
-      Post List page
+      <h1>Post List page</h1>
+
+      <ul>
+        {posts.map(post => <li key={post.id}>{post.title}</li>)}
+      </ul>
     </div>
   );
 }
+
+// Hàm bên dưới chỉ chạy bên phía server, phía client sẽ không chạy
+export const getStaticProps: GetStaticProps<PostListPageProps> = async (context: GetStaticPropsContext) => {
+  // server side
+  // builde-time 
+
+  console.log('static prop');
+  const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const data = await response.json();
+
+  console.log(data);
+
+  return {
+    props: {
+      posts: data.map((item: any) => ({ id: item.id, title: item.title }))
+    }
+  }
+}
+
+// export const getServerSideProps = () => {
+//   return {}
+// }
